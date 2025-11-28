53a993466767a72fece81079dd21337acd4f613c
C. Liam Brown <liam@cliambrown.com>
2025-11-28 11:41:28 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Taurimoji
+
+A simple emoji picker desktop application built with Tauri.
+
+Uses [the complete v17.0 emoji list](https://unicode.org/emoji/charts/full-emoji-list.html) (with a few taken out because they seemed buggy on my machine).
+
+Made by [C. Liam Brown](https://cliambrown.com/)
+
+## Installation
+
+1. Clone the repo to your machine
+2. Follow the instructions at https://v2.tauri.app/distribute/#distributing to build an installer
+  - `/src-tauri/tauri.conf.json` is currently configured to build only a .deb installer file (for Linux operating systems); you may need to make adjustments for your specific device
+3. Install Taurimoji
+4. Optional but pretty sweet: Create a custom keybinding to open Taurimoji
+
+## Usage
+
+- Type an emoji name to filter the list of emojis
+- Click an emoji to copy it to your clipboard and close the app
+- Shift + click an emoji to copy multiple emojis without closing the app
+- Tab = highlight the next emoji
+- Shift + Tab = highlight the previous emoji
+- Enter = copy the highlighted emoji and close the app
+- Shift + Enter = copy multiple emojis without closing the app
+- Ctrl + W = close the app
+- Esc = Clear the filter input
+
+## Built With
+
+- [Tauri](https://tauri.app/)
+- [Vue](https://vuejs.org/)
+- [Tailwind](https://tailwindcss.com/)
+
+## Disclaimer
+
+This software is provided as-is. Its developer makes no other warranties, express or implied, and hereby disclaims all implied warranties, including any warranty of merchantability and warranty of fitness for a particular purpose.
---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<script setup>
+import { ref, computed, watch, useTemplateRef, onBeforeMount, onBeforeUnmount, onMounted } from 'vue';
+import copy from 'copy-text-to-clipboard';
+import { exit } from '@tauri-apps/plugin-process';
+
+import emojis from './emoji.json';
+
+const input = useTemplateRef('input');
+
+onMounted(() => {
+  input.value.focus();
+});
+
+const filter = ref('');
+const highlightedIndex = ref(0);
+let selectedText = '';
+
+let ctrlIsDown = false;
+let shiftIsDown = false;
+
+const filteredEmojis = computed(() => {
+  if (!filter.value) return emojis;
+  const filterL = filter.value.toLowerCase();
+  return emojis.filter(emoji => emoji.name.includes(filterL));
+});
+
+watch(
+  () => filteredEmojis.value.length,
+  (length) => highlightedIndex.value = 0
+);
+
+function selectEmoji(emoji, index) {
+  if (!emoji) emoji = filteredEmojis.value[index];
+  highlightedIndex.value = index;
+  if (shiftIsDown) {
+    selectedText = selectedText + emoji.emoji;
+    copy(selectedText);
+  } else {
+    copy(emoji.emoji);
+    exit(0);
+  }
+}
+
+function emojiNav(back = false) {
+  if (back) {
+    if (highlightedIndex.value > 0) highlightedIndex.value--;
+  } else {
+    if (highlightedIndex.value < filteredEmojis.value.length - 1) highlightedIndex.value++;
+  }
+}
+
+function handleKeydown(event) {
+  
+  // event.key = "Unidentified" for shift+tab for some reason
+  if (event.keyCode === 9 && event.shiftKey) {
+    event.preventDefault();
+    emojiNav(true);
+  }
+  
+  switch (event.key) {
+    case 'Control':
+      ctrlIsDown = true;
+      break;
+    case 'Shift':
+      shiftIsDown = true;
+      break;
+    case 'w':
+      if (ctrlIsDown) {
+        event.preventDefault();
+        exit(0);
+      }
+      break;
+    case 'Escape':
+      event.preventDefault();
+      filter.value = '';
+      input.value.focus();
+      break;
+    case 'Tab':
+      event.preventDefault();
+      if (shiftIsDown) emojiNav(true);
+      else emojiNav();
+      break;
+    case 'Enter':
+      event.preventDefault();
+      selectEmoji(null, highlightedIndex.value);
+      break;
+  }
+  return true;
+}
+
+function handleKeyup(event) {
+  if (event.key === 'Control') ctrlIsDown = false;
+  else if (event.key === 'Shift') shiftIsDown = false;
+}
+
+onBeforeMount(() => {
+  window.addEventListener('keydown', handleKeydown);
+  window.addEventListener('keyup', handleKeyup);
+});
+onBeforeUnmount(() => {
+  window.removeEventListener('keydown', handleKeydown);
+  window.removeEventListener('keyup', handleKeyup);
+});
+
+</script>
+
+<template>
+  <main class="w-full h-full p-4 flex flex-col max-h-full">
+    
+    <div>
+      <input type="text" v-model="filter" ref="input" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+    </div>
+    
+    <div class="grow py-2 -mx-1 overflow-y-auto text-center" style="min-height:1px;">
+      
+      <div v-for="(emoji, index) in filteredEmojis" class="inline-block m-1">
+        <button type="button" class="text-3xl cursor-pointer p-1 rounded-md" :class="{ 'bg-gray-300': index == highlightedIndex }" @click="selectEmoji(emoji, index)" :title="emoji.name">
+          {{ emoji.emoji }}
+        </button>
+      </div>
+      
+    </div>
+    
+  </main>
+</template>
